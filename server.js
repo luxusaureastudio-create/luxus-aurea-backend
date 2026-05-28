81f5701ca8f30d1d1484a501b269c79fd547c59b
Luxus Aurea <luxusaureastudio@gmail.com>
2026-05-28 23:42:14 +0200

    Impostato 1 credito gratuito di default

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const User = mongoose.model('User', new mongoose.Schema({
     companyName: String,
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    credits: { type: Number, default: 10 }
+    credits: { type: Number, default: 1 }
 }));
 
 const Report = mongoose.model('Report', new mongoose.Schema({
